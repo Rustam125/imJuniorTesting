b3940b6c5cfd35a271efe396186a01ae37c57386
Гараев Рустам <r.garaev@reo.ru>
2023-08-05 18:41:01 +0400

    init

---
+++ imJuniorTesting/imJuniorTesting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace imJuniorTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
